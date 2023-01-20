f2399480c598ffb439f509b5c7bc4567c22f4e34
Katherine <katherinejc103@gmail.com>
2023-01-19 20:29:36 -0500

    commit tree d0537bae80cc752173b047956d47d251d8f3ebbe

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,8 @@
 class Passenger < ApplicationRecord
+    has_many :ride
+    has_many :taxi through: :ride
+
+    def change 
+        add_columnn :passengers, passenger_id, integer
+    end 
 end
--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,2 +1,9 @@
 class Taxi < ApplicationRecord
+    has_many :ride
+    has_many :passenger through: :ride
+
+
+    def change 
+        add_columnn :taxis, taxi_id, integer
+    end 
 end
